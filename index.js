9a4e1e37abf0d08f45e78c7cc5eeddc1870dd27f
sudhakar1999 <53487470+sudhakar1999@users.noreply.github.com>
2022-01-26 18:24:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var a=10
+if(a>=10){
+
+console.log("greater or equal to 10");
+}
+else
+{
+cosole.log("lower than 10");
+}
+
+var i=16
+var j=14
+var k=13
+if(i>=j && i>=k)
+{
+console.log(i+" is greater");
+}
+else if(j>=k)
+{
+console.log(j+" is greater");
+}
+else
+{
+console.log(k+" is greater");
+}
